a86efd1aafd6800227c7a643e02ecbce907291eb
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2023-03-20 12:18:42 -0400

    Update color.py

--- color.py
+++ color.py
@@ -8,3 +8,16 @@ RESET = "\u001b[0m"
 
 def cprint(text,fg=(255,255,255),bg=(0,0,0),**kwargs):
 	print(rgbf(*fg)+rgbb(*bg)+text+RESET,**kwargs)
+
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+yellow = (255,255,0)
+teal = (0,255,255)
+purple = (255,0,255)
+
+orange = (255,125,0)
+pink = (255,0,145)
